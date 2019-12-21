ef4edb9c76794eeca97b7451cd47b9a5b66ee9e0
Jared Sinclair <jaredsinclair.rn@gmail.com>
2019-12-21 01:31:02 -0600

    Add a view controller file.

---
+++ UIViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController.swift
+//  Etcetera
+//
+//  Created by Jared Sinclair on 12/21/19.
+//  Copyright © 2019 Nice Boy, LLC. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    public static func instantiateFromStoryboard<T: UIViewController>(of type: T.Type = T.self) -> T {
+        let identifier = String(describing: type)
+        let storyboard = UIStoryboard(name: identifier, bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: identifier)
+        return viewController as! T
+    }
+
+}
