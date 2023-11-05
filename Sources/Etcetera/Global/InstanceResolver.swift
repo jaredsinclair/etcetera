d76c1f2d4b71c59a457c2620b83300d98ac7e9b3
Jared Sinclair <jaredsinclair.rn@gmail.com>
2023-11-05 13:40:51 -0600

    Fix recursion bug.

--- Sources/Etcetera/Global/InstanceResolver.swift
+++ Sources/Etcetera/Global/InstanceResolver.swift
@@ -10,19 +10,19 @@
 @MainActor @usableFromInline final class InstanceResolver {
 
     /// A single resolved instance, not keyed by any instance identifier.
-    private var resolvedInstance = Protected<Any?>(nil)
+    private var resolvedInstance: Any?
 
     /// Instances keyed by instance identifiers.
-    private var resolvedInstances = Protected<[AnyHashable: Any]>([:])
+    private var resolvedInstances: [AnyHashable: Any] = [:]
 
     /// Initializes a resolver with an initial value.
     @usableFromInline init<T: GloballyAvailable>(resolvedValue: T) {
-        resolvedInstance.access { $0 = resolvedValue }
+        resolvedInstance = resolvedValue
     }
 
     /// Initializes a resolver with an initial value for `identifier`.
     @usableFromInline init<T: GloballyIdentifiable>(resolvedValues: [T.InstanceIdentifier: T]) {
-        resolvedInstances.access { $0 = resolvedValues }
+        resolvedInstances = resolvedValues
     }
 
     /// Initializes an empty resolver.
@@ -30,62 +30,54 @@
 
     /// Resolves an instance of `T` using `GlobalContainer`.
     @usableFromInline func resolved<T: GloballyAvailable>(container: Container) -> T {
-        return resolvedInstance.access { instance -> T in
-            if let existing = instance as? T {
-                return existing
-            } else {
-                let new = T.make(container: container)
-                instance = new
-                return new
-            }
+        if let existing = resolvedInstance as? T {
+            return existing
+        } else {
+            let new = T.make(container: container)
+            resolvedInstance = new
+            return new
         }
     }
 
     /// Resolves a specific instance of `T` for a given identifier.
     @usableFromInline func resolved<T: GloballyIdentifiable>(container: Container, identifier: T.InstanceIdentifier) -> T {
         let key = AnyHashable(identifier)
-        return resolvedInstances.access { dictionary -> T in
-            if let existing = dictionary[key] as? T {
-                return existing
-            } else {
-                let new = T.make(container: container, identifier: identifier)
-                dictionary[key] = new
-                return new
-            }
+        if let existing = resolvedInstances[key] as? T {
+            return existing
+        } else {
+            let new = T.make(container: container, identifier: identifier)
+            resolvedInstances[key] = new
+            return new
         }
     }
 
     /// Resolves an instance of `T` using `GlobalContainer`.
     @usableFromInline func resolved<T>(via initializer: @MainActor (Container) -> T, container: Container) -> T {
-        return resolvedInstance.access { instance -> T in
-            if let existing = instance as? T {
-                return existing
-            } else {
-                let new = initializer(container)
-                instance = new
-                return new
-            }
+        if let existing = resolvedInstance as? T {
+            return existing
+        } else {
+            let new = initializer(container)
+            resolvedInstance = new
+            return new
         }
     }
 
     /// Resolves an instance of `T` using `GlobalContainer`.
     @usableFromInline func resolved<T, InstanceIdentifier: Hashable>(for identifier: InstanceIdentifier, via initializer: @MainActor (Container) -> T, container: Container) -> T {
         let key = AnyHashable(identifier)
-        return resolvedInstances.access { dictionary -> T in
-            if let existing = dictionary[key] as? T {
-                return existing
-            } else {
-                let new = initializer(container)
-                dictionary[key] = new
-                return new
-            }
+        if let existing = resolvedInstances[key] as? T {
+            return existing
+        } else {
+            let new = initializer(container)
+            resolvedInstances[key] = new
+            return new
         }
     }
 
     /// Removes a previously-resolved instance for `identifier`, if any.
     @usableFromInline func removeInstance<T: GloballyIdentifiable>(of type: T.Type, for identifier: T.InstanceIdentifier) {
         let key = AnyHashable(identifier)
-        resolvedInstances.access { $0[key] = nil }
+        resolvedInstances[key] = nil
     }
 
 }
