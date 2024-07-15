5ca82654ee3e1ee6321bbc520ba7e05731fddb6e
Jared Sinclair <jaredsinclair.rn@gmail.com>
2024-07-14 20:33:34 -0500

    Enable strict concurrency checking, momentarily.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.10
 
 import PackageDescription
 
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(
             name: "Etcetera",
-            swiftSettings: [ .swiftLanguageVersion(.v6) ]
+            swiftSettings: [ .enableExperimentalFeature("StrictConcurrency") ]
         ),
         .testTarget(
             name: "EtceteraTests",
